da295b3e0d3ad85d25cefe6c3a2c023f4203e120
GallantArc302 <GallantArc302@gmail.com>
2026-04-25 15:08:44 -0400

    support spaces

--- _run.bat
+++ _run.bat
@@ -1,2 +1,2 @@
-python nintendoseq2midi.py %1 %~n1.mid
+python nintendoseq2midi.py "%1" "%~n1.mid"
 pause
--- _runALL.bat
+++ _runALL.bat
@@ -1,3 +1,3 @@
 for %%i in (*.bsseq *.brseq *.bcseq *.bfseq *.bseq) do (
-	python nintendoseq2midi.py %%i %%~ni.mid
+	python nintendoseq2midi.py "%%i" "%%~ni.mid"
 )
